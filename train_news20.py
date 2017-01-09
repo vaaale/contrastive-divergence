055ed9bb25e0bfb8d9b4e3d420a251ebdce96161
Alexander Vaagan <alexander.vaagan@gamil.com>
2017-01-09 10:49:55 +0100

    Timing

--- train_news20.py
+++ train_news20.py
@@ -2,16 +2,24 @@ from news20.dataset import news20_minibatches, news20_data
 from news20.finetune import finetune_news20
 from news20.pretrain import pretrain_news20
 from news20.visualize_news20 import visualize
+import time
 
 
 def train_news20():
     batch_size = 100
 
     x_train = news20_minibatches(batch_size)
+    pt_start = time.time()
     pretrain_news20(x_train, 'news20/models')
+    pt_end = time.time()
 
     x_train, labels = news20_data()
+    ft_start = time.time()
     finetune_news20(x_train, 'news20/models')
+    ft_end = time.time()
+
+    print('Pretraining: {0}'.format(pt_end-pt_start))
+    print('Finetuning: {0}'.format(ft_end-ft_start))
 
     visualize(x_train, labels)
 
